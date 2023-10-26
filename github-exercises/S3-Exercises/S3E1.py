3ceacdeaccec8bb38d2495492fb371b5440c2d73
Sue Lim <sue.p.lim@gmail.com>
2023-10-26 14:29:10 +0800

    New file - Random Password Generator
    Committing code added to a file in for a password generator.

--- github-exercises/S3-Exercises/S3E1.py
+++ github-exercises/S3-Exercises/S3E1.py
@@ -9,3 +9,30 @@ Use the random module
 Keep it simple at this stage
 
 Commit to your repository and push to GitHub"""
+
+# Import the random module, which allows us to generate random values.
+import random
+import string
+
+# Import constants from the string module for letters, digits, and special characters.
+letters = string.ascii_letters  # Uppercase and lowercase letters
+digits = string.digits  # Numeric digits (0-9)
+special_char = string.punctuation  # Special characters like !@#$%^&*()
+
+# Concatenate the above string constants to create a character set.
+alphabet = letters + digits + special_char
+
+# Define the desired password length.
+password_length = 10
+
+# Create an empty string to store the generated password.
+password = ""
+
+# Loop through the  password length and append random characters to the password string.
+for i in range(password_length):
+    password += random.choice(alphabet)
+
+# Print the randomly generated password.
+print("Here is the randomly generated password:", password)
+
+# ref: https://geekflare.com/password-generator-python-code/
