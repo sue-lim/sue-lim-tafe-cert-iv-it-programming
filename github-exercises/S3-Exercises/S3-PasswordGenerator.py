2f524afa833177975456fe874ae0a2c527a49b7f
Sue Lim <sue.p.lim@gmail.com>
2023-10-26 17:02:25 +0800

    User Input and Input Validations added
    Added a feature to your password generator:
    *User Input
    *Input Validation

--- github-exercises/S3-Exercises/S3-PasswordGenerator.py
+++ github-exercises/S3-Exercises/S3-PasswordGenerator.py
@@ -2,20 +2,38 @@
 import random
 import string
 
-# creating alphabet variable utlilist string import
+# Import constants from the string module for letters
 alphabet = string.ascii_letters
 
-# Define the desired password length.
-password_length = 10
+# While loop to Validate the input: 0 < length < 50
+
+while True:
+    print("Enter Password Length:")
+    passwordLen = input()
+    try:
+        passwordLen = int(passwordLen)
+    except:
+        print("Please use numeric digits.")
+        continue
+    if passwordLen <= 0:
+        print("Invalid")
+        continue
+    if passwordLen >= 50:
+        print("Invalid")
+        continue
+    break
 
 # Create an empty string to store the generated password.
 password = ""
 
 # Loop through the  password length and append random characters to the password string.
-for i in range(password_length):
+for i in range(passwordLen):
     password += random.choice(alphabet)
 
 # Print the randomly generated password.
-print("Here is the randomly generated password:", password)
+# Generate and print a string of {length} random characters
+print(f"Your password is {password}.")
+
 
 # ref: https://geekflare.com/password-generator-python-code/
+# try / except / continue - https://automatetheboringstuff.com/2e/chapter8/
