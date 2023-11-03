f333a1562aa1d63dc0305b535ed004ada37dc477
Sue Lim <sue.p.lim@gmail.com>
2023-11-03 15:59:32 +0800

    fixing conflicts

--- python_exercises/S11_Exercises_Wordle/wordle.py
+++ python_exercises/S11_Exercises_Wordle/wordle.py
@@ -1,43 +1,43 @@
 import random
-import pathlib
-
-
-hintwordlist = pathlib.Path("./word-bank/all_words.txt")
+from pathlib import Path
 
 
 def game_introduction():
-    name = str(
+    user_name = str(
         input(
             "Welcome to Wordle\nLet's get to know each other a little more before we get started!\nWhat's your name? "
         )
     )
-    game_instructions = "Wordle is a single player game\nA player has to guess a five letter hidden word\nYou have 6 attempts\nYour Progress Guide\nIndicates that the letter as that position is in the hidden word but in a different position (TO BE UPDATED)\nIndicates that the letter at that position is in the hidden word...(TO BE UPDATED)\n\nLET'S GET STARTED!"
-    print(f"Hey {name}\n{game_instructions}\n")
+    game_instructions_message = "Wordle is a single player game\n\nA player has to guess a five letter hidden word\nYou have 6 attempts\nYour Progress Guide\nIndicates that the letter as that position is in the hidden word but in a different position (TO BE UPDATED)\nIndicates that the letter at that position is in the hidden word...(TO BE UPDATED)\n"
+    print(f"Hey {user_name}... Nice to E-Meet you!\n\n{game_instructions_message}\n")
 
 
 def get_random_word():
-    wordlist = pathlib.Path(__file__).parent / "./word-bank/target_words.txt"
-    words = [
-        word.upper()
-        for word in wordlist.read_text(encoding="utf-8").strip().split("\n")
+    target_word_list = Path(__file__).parent / "./word-bank/target_words.txt"
+    target_words = [
+        target_word.upper()
+        for target_word in target_word_list.read_text(encoding="utf-8")
+        .strip()
+        .split("\n")
     ]
-    return random.choice(words)
+    # print(target_words)
+    return random.choice(target_words)
 
 
-def show_guess(guess, word):
+def show_guess(guess, target_word):
     correct_letters = {
-        letter for letter, correct in zip(guess, word) if letter == correct
+        letter for letter, correct in zip(guess, target_word) if letter == correct
     }
-    misplaced_letters = set(guess) & set(word) - correct_letters
-    wrong_letters = set(guess) - set(word)
+    misplaced_letters = set(guess) & set(target_word) - correct_letters
+    wrong_letters = set(guess) - set(target_word)
 
     print("Correct  letters:", ", ".join(sorted(correct_letters)))
     print("Misplaced letters:", ", ".join(sorted(misplaced_letters)))
     print("Wrong letters:", ", ".join(sorted(wrong_letters)))
 
 
-def game_over(word):
-    print(f"The word was {word}")
+def game_over(target_word):
+    print(f"The word was {target_word}")
 
 
 game_introduction()
@@ -65,3 +65,5 @@ def main():
 # THIS LINE MAKES SURE YOUR CODE IS CALLED WHEN THE FILE IS EXECUTED
 if __name__ == "__main__":
     main()
+
+# TODO - PLAY AGAIN
