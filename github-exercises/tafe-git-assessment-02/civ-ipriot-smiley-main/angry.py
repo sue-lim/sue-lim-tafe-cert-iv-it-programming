b7734580ca59da42ea46990c2f792eb3d5a67516
Sue Lim <sue.p.lim@gmail.com>
2023-11-12 22:33:00 +0800

    New Update - Angry.py

---
+++ github-exercises/tafe-git-assessment-02/civ-ipriot-smiley-main/angry.py
@@ -0,0 +1,48 @@
+import time
+from smiley import Smiley
+
+
+class Angry(Smiley):
+    """
+    Angry is a subclass of Smiley"""
+
+    def __init__(self):
+        super().__init__(complexion=self.RED)
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        """
+        Method that draws the mouth on the standard faceless smiley.
+        """
+        mouth = [49, 54, 42, 43, 44, 45]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        """
+        Method that draws the eyes (open or closed) on the standard smiley.
+        :param wide_open: True if eyes opened, False otherwise
+        """
+
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            self.pixels[pixel] = self.BLANK if wide_open else self.complexion()
+
+    def blink(self, delay=0.25):
+        """
+        Make the sad face blink once with a certain delay (in s).
+
+        :param delay: Delay in seconds
+        """
+        # CLOSE EYES TO SIMULATE A BLINK
+        self.draw_eyes(wide_open=False)
+        # DISPLAY SAD FACE WITH THE CLOSED EYES
+        self.show()
+        # PAUSE FOR DELAY SET ABOVE
+        time.sleep(delay)
+        # OPEN EYES TO SIMULATE THE END OF THE BLINK
+        self.draw_eyes(wide_open=True)
+        # DISPLAY SAD FACE WITH OPEN EYES
+        self.show()
